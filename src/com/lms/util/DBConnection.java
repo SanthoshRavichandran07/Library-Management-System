ad833f0dd7fd5698e78e0a32b9aaa5e025ef19a8
ramprasathmk <mkramprasath12@gmail.com>
2026-02-12 11:54:21 +0530

    feat: hikari db connection pool concept added

--- src/com/lms/util/DBConnection.java
+++ src/com/lms/util/DBConnection.java
@@ -7,10 +7,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class DBConnection {
+
+	private static final HikariDataSource dataSource;
 	
+	// * Method 1: using .properties file.
 	public static Connection getConnection() {
 		
 		Connection con = null;
@@ -38,6 +46,7 @@ public class DBConnection {
 		return con;
 	}
 
+	// Method 2: using .env file
 	/**
 	 * Retrieving the Database Configurations from Environment file for security purpose.
 	 * Using the Database Connection for better efficiency.
@@ -52,16 +61,43 @@ public class DBConnection {
 		return DriverManager.getConnection(URL, USER, PASS);
 	}
 
+	// Method 3: using The HikariConfig for Database Connection Pool.
+	static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();      
+        String db_url = dotenv.get("DB_URL");
+        String db_user = dotenv.get("DB_USERNAME");
+        String db_pass = dotenv.get("DB_PASSWORD");
+        
+        if (db_url == null || db_user == null || db_pass == null) {
+            throw new IllegalStateException("Database configuration missing. Please set DB_URL, DB_USER, and DB_PASS as environment variables.");
+        }
+        
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(db_url);
+        config.setUsername(db_user);
+        config.setPassword(db_pass);
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setIdleTimeout(30000);
+        config.setConnectionTimeout(30000);
+        
+        dataSource = new HikariDataSource(config);
+    }
+    
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+
 	/// NOTE: This main method is only for Testing the Local Database Connection.
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, ClassNotFoundException {
 		Connection c = null;
 		try {
 			System.out.println("Connecting to DB");
 			Thread.sleep(1000);
-			c = connectDB();
+			c = getDataSource().getConnection();
 			System.out.println("Connected to Database");
 			Thread.sleep(1000);
-		} catch (ClassNotFoundException | SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
